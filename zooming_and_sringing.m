82c219f60b71499f785806a02b99afbc6c20e82a
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2024-04-26 14:49:38 +0600

    Add files via upload

---
+++ zooming_and_sringing.m
@@ -0,0 +1,35 @@
+clc;
+clear;
+close all;
+
+a = imread('image.jpg');
+
+a = 0.59 * a(:,:,1) + 0.3 * a(:,:,2) + 0.11 * a(:,:,3);
+
+f = 2;
+
+[n,m] = size(a);
+
+for i=1:n
+    for j=1:m
+        for k =1:f
+            b(i,(j-1)*f + k) = a(i,j);
+        end
+    end
+end
+
+c = b;
+[n,m] = size(c);
+
+for i=1:m
+    for j=1:n
+        for k=1:f
+            b((j-1)*f+k,i) = c(j,i);
+        end
+    end
+end
+
+figure, imshow(a);
+figure, imshow(b);
+
+
