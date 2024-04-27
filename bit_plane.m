f3e3e892f8d6eaecac600215a2453d0429649a90
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2024-04-27 16:39:58 +0600

    Update bit_plane.m

--- bit_plane.m
+++ bit_plane.m
@@ -5,17 +5,17 @@ close all;
 image = imread('black.jpg');
 image = rgb2gray(image);
 image = double(image);
-b = mod(image,2);
 
 len = 8;
 figure;
-subplot(331);
+subplot(3,3,1);
 imshow(uint8(image));
-for i=1:len
+
+for i = 1:len
     b = mod(image,2);
     subplot(3,3,i+1);
     imshow(b);
-    title(sprintf('Bit plane %d', i))
+    title(sprintf('Bit plane %d', i));
     image = floor(image/2);
 end
 
